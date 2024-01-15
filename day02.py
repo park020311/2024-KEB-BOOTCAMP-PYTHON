0c1977822ec5321739422a528c6dd3dfeab68f51
park020311 <zxwrtygsnm@naver.com>
2024-01-15 13:21:50 +0900

    v0.7 day02

--- day02.py
+++ day02.py
@@ -5,9 +5,14 @@
 # print(cash)
 # print(type(cash)) # int
 
+#f-string
 base_number = int(input('Input base number : '))
 exponent_number = int(input('Input exponent number : '))
-print(f'밑은 {base_number},지수는 {exponent_number},결과 값은 {base_number**exponent_number}')
+# print(f'밑은 {base_number},지수는 {exponent_number},결과 값은 {base_number**exponent_number}')
+print(f'밑은 {base_number},지수는 {exponent_number},결과 값은 {pow(base_number,exponent_number)}')
 
+#format함수
+print('밑은 {0},지수는 {1},결과 값은 {2}'.format(base_number,exponent_number,base_number**exponent_number))
 
-
+#c like
+print('밑은 %d,지수는 %d,결과값은 %d' % (base_number,exponent_number,pow(base_number,exponent_number)))
