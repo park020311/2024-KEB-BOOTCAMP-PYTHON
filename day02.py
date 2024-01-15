67445a4a984eb1ad09cf8eca8115d298d95ffd8d
park020311 <zxwrtygsnm@naver.com>
2024-01-15 11:43:37 +0900

    v0.5 python day02

--- day02.py
+++ day02.py
@@ -1,17 +1,21 @@
-# univ = "Inha University"
-# univ[5] = 'U'
-# print(univ)
-# subjects = ['Python','C++','linux','datas structure','database']
-
 # SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
 #
 # "univ" = "university"
 
-#0k
-#case-sensitive
+# 0k
+# case-sensitive
 # abc = 7
 # Abc = 8
 # ABC = 6
 # print(abc, Abc, ABC)
+
+# artist = ['BTS', 'New Jeans', 'SES', 'Black Pink']
+# groups = artist
+# print(artist, groups)
+# artist[2] = 'seven teen'
+# print(artist, groups)
+
+
+
 
 
