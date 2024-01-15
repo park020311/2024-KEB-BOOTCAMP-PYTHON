42a4ef5f639d02a21d911f04699aeb1f95cbce96
park020311 <zxwrtygsnm@naver.com>
2024-01-15 10:55:45 +0900

    v0.3

--- day02.py
+++ day02.py
@@ -1,14 +1,17 @@
-# print(0.1)
-# print(1e-1)
-# print(0.01)
-# print(1e-2)
-# print(3.14)
-# print(314.1592)
-# print(0.3141592e3)
-# print(21000)
-# print(21_000)
-univ = "Inha University"
-univ[5] = 'U'
-print(univ)
-subjects = ['Python','C++','linux','datas structure','database']
+# univ = "Inha University"
+# univ[5] = 'U'
+# print(univ)
+# subjects = ['Python','C++','linux','datas structure','database']
+
+# SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+#
+# "univ" = "university"
+
+#0k
+#case-sensitive
+# abc = 7
+# Abc = 8
+# ABC = 6
+# print(abc, Abc, ABC)
+
 
