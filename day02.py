d95704fbcdcddf91588e3b037d5d76068ff92dad
park020311 <zxwrtygsnm@naver.com>
2024-01-15 14:14:52 +0900

    v0.9 day02

--- day02.py
+++ day02.py
@@ -1,11 +1,7 @@
-# first_number = int(input("first number : "))
-# second_number=int(input("second number : "))
-#
-# quotient = first_number // second_number
-# remainder = first_number % second_number
-# print(f'목은 {quotient} 나머지는 {remainder}입니다')
-
-first_number = int(input("first number : "))
-second_number=int(input("second number : "))
-
-print(f'몫은 {divmod(first_number, second_number)[0]} 나머지는 {divmod(first_number, second_number)[1]}입니다')
+dec =65
+octal = 0o101
+hex = 0x41
+binary =0b01000001
+print(dec,octal,hex,binary)
+print(chr(dec),chr(octal),chr(hex),chr(binary))
+print(bin(dec),bin(octal),bin(hex),bin(binary))
