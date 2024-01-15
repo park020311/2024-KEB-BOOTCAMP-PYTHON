4e57054aeee0827025c7fbfe862d13440130d00a
park020311 <zxwrtygsnm@naver.com>
2024-01-15 14:29:41 +0900

    0.9v day 02

--- day02.py
+++ day02.py
@@ -1,7 +1,2 @@
-dec =65
-octal = 0o101
-hex = 0x41
-binary =0b01000001
-print(dec,octal,hex,binary)
-print(chr(dec),chr(octal),chr(hex),chr(binary))
-print(bin(dec),bin(octal),bin(hex),bin(binary))
+fahrenheit = float(input('Input Fahrenheit : '))
+print(f'Fahrenheit : {fahrenheit}F, Celsius :{((fahrenheit-32.0)*5.0/9.0):.4f}C')
