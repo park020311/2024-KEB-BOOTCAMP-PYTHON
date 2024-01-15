8c81b8904edf36f0efdbedb89a4499a7868469ce
park020311 <zxwrtygsnm@naver.com>
2024-01-15 15:47:14 +0900

    v1.2 day02

--- day02.py
+++ day02.py
@@ -1,15 +1,8 @@
-# sum =1 + \
-#     2 + \
-#     3 + \
-#     4
-# print(sum)
-
-# temp = [0]
-# temp = []
-
-temp = []
-if temp:
-    print("원소가 존재하는 리스트")
+# vowels = {'a', 'e', 'i', 'o', 'u'}
+vowels = "aeiuo"
+letter = input("input alphabet  : ") # set
+print(type(vowels))
+if letter in vowels:
+    print(f'{letter} is a vowel~')
 else:
-    print("비어있는 리스트")
-
+    print(f'{letter} is a consonant!')
