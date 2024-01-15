887497086db9c475724769bcff4d2b5cf6be768b
park020311 <zxwrtygsnm@naver.com>
2024-01-15 14:55:23 +0900

    v1.0 day02

--- day02.py
+++ day02.py
@@ -1,2 +1,12 @@
-fahrenheit = float(input('Input Fahrenheit : '))
-print(f'Fahrenheit : {fahrenheit}F, Celsius :{((fahrenheit-32.0)*5.0/9.0):.4f}C')
+menu = input("1. Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3. Quit program")
+
+if menu == '1':
+    fahrenheit = float(input('Input Fahrenheit : '))
+    print(f'Fahrenheit : {fahrenheit}F, Celsius :{((fahrenheit-32.0)*5.0/9.0):.4f}C')
+elif menu == "2":
+    celsius = float(input('Input Fahrenheit : '))
+    print(f'Fahrenheit : {celsius}F, Celsius :{((celsius * 9.0 / 5.0) +32.0):.4f}C')
+
+
+
+
