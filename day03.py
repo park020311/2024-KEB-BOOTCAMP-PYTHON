7dd290e4def80c2069ba898480ba65c287d50a04
park020311 <zxwrtygsnm@naver.com>
2024-01-16 10:34:38 +0900

    v0.1 day03

---
+++ day03.py
@@ -0,0 +1,10 @@
+#raw string
+# university = r"inha\nuniversity"
+# print(university)
+
+#slicing
+university = "inha university"
+print(university[1:6])
+print(university[:-11])
+print(university[::2]) # 두번 째 : 오른쪽에는 증감식
+print(university[:len(university)])
