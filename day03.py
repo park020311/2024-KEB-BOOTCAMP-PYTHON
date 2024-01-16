0f1facd65d0d82a73326aca9a0f429bc9a62a10e
park020311 <zxwrtygsnm@naver.com>
2024-01-16 11:39:47 +0900

    v0.4 day 03

--- day03.py
+++ day03.py
@@ -1,16 +1,6 @@
-# course = "2024 KEB Bootcamp"
-# print(course.replace('KEB','Inha'))
-# print(course)
-# course=course.replace('KEB','Inha')
-# print(course)
-
-course = "* KEB 2024# KEB !Bootcamp KEB....*!#"
-print(course.find("KEB"))
-print(course.rfind("KEB")) #r은 뒤에서부터
-print(course.index("KEB"))
-print(course.rindex("KEB"))
-print(course.find('Inha')) # -1
-print(course.index('Inha')) # ValueError: substring not found
-print(course.replace('KEB','Inha', 2)) # 3번째에는 replace를 몇 번 할지 횟수이다.
-print(course.strip())
-print(course.strip("!#.*")) #  양쪽 끝은 연속적으로 없애지만 아닌것들은 남긴다.
+subjects = "python c++ database linux"
+subject = input("수강신청 과목 : ")
+if subjects.find(subject) != -1:
+    print(f"해당 과목은 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.")
+else:
+    print("해당 과목이 존재하지 않습니다.")
