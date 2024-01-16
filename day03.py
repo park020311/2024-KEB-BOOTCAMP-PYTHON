85193ecbfd09375f7dff2bcd4335a7470a4ba049
park020311 <zxwrtygsnm@naver.com>
2024-01-16 11:34:43 +0900

    v0.3 day03

--- day03.py
+++ day03.py
@@ -1,14 +1,16 @@
 # course = "2024 KEB Bootcamp"
+# print(course.replace('KEB','Inha'))
 # print(course)
-# # list_course = course.split() # ()안에 아무것도 입력안하면 디폴트값이 공백이다.
-# list_course = course.split('B')
-# print(list_course)
+# course=course.replace('KEB','Inha')
+# print(course)
 
-# numbers =input("FirstNumber SecondNumber : ").split()
-# print(numbers[0]+numbers[1]) # concatenation
-# print(int(numbers[0])+int(numbers[1])) # arithmetic operation
-
-subjects = ["python", "c++", "database"]
-subjects_string = " / ".join(subjects)
-print(subjects_string)
-
+course = "* KEB 2024# KEB !Bootcamp KEB....*!#"
+print(course.find("KEB"))
+print(course.rfind("KEB")) #r은 뒤에서부터
+print(course.index("KEB"))
+print(course.rindex("KEB"))
+print(course.find('Inha')) # -1
+print(course.index('Inha')) # ValueError: substring not found
+print(course.replace('KEB','Inha', 2)) # 3번째에는 replace를 몇 번 할지 횟수이다.
+print(course.strip())
+print(course.strip("!#.*")) #  양쪽 끝은 연속적으로 없애지만 아닌것들은 남긴다.
