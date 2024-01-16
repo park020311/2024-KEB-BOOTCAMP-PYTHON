f576a7f15e71e33825a444eaf1fc44a4927373f9
park020311 <zxwrtygsnm@naver.com>
2024-01-16 10:58:28 +0900

    v0.2 day03

--- day03.py
+++ day03.py
@@ -1,10 +1,14 @@
-#raw string
-# university = r"inha\nuniversity"
-# print(university)
+# course = "2024 KEB Bootcamp"
+# print(course)
+# # list_course = course.split() # ()안에 아무것도 입력안하면 디폴트값이 공백이다.
+# list_course = course.split('B')
+# print(list_course)
 
-#slicing
-university = "inha university"
-print(university[1:6])
-print(university[:-11])
-print(university[::2]) # 두번 째 : 오른쪽에는 증감식
-print(university[:len(university)])
+# numbers =input("FirstNumber SecondNumber : ").split()
+# print(numbers[0]+numbers[1]) # concatenation
+# print(int(numbers[0])+int(numbers[1])) # arithmetic operation
+
+subjects = ["python", "c++", "database"]
+subjects_string = " / ".join(subjects)
+print(subjects_string)
+
