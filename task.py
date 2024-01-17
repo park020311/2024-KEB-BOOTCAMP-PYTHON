2e3bbc8e5144b6513a6dfe1ad9884f434e39151b
park020311 <zxwrtygsnm@naver.com>
2024-01-17 20:49:33 +0900

    v0.2 day04 task #8.6~#8.10

--- task.py
+++ task.py
@@ -1,13 +1,13 @@
-#8.1
-e2f={"dog" : "chein","cat" : "chat","walrus" : "morse" }
-print(e2f)
-#8.2
-print(e2f["walrus"])
-#8.3
-f2e=dict(e2f.items())
-print(f2e)
-#8.4
-print(list(e2f.keys())[0])
-#8.5
-for eng in e2f:
-    print(eng)
+#8.6
+life ={"animals" : {"cats" : "Henry", "octopi" : "Grumpy", "emus" : "Lucy"},"plants" : {}, "others" : {}}
+#8.7
+for high_key in life:
+    print(high_key)
+#8.8
+
+print(life["animals"].keys())
+#8.9
+print(life["animals"].values())
+print(life["animals"]["cats"]) # 여기서는 왜 values()를 붙이면 안되는지?
+#8.10
+#squares = {range(10):range(10)**2 for range(10)} # 시간이 없어서 집가서 마저 공부
