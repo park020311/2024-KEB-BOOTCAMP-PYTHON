83bf721026c5b57fb9b6492ca27d3a2efa3b43dc
park020311 <zxwrtygsnm@naver.com>
2024-01-17 15:44:33 +0900

    v0.8 day04

--- day04.py
+++ day04.py
@@ -1,20 +1,32 @@
-sugang = dict(python= "kim", cpp = "sung", db = "kang")
-# print(sugang)
-# sugang['datastructure'] = 'kim' # add
-# print(sugang)
-# sugang['datastructure'] = 'park' # update
-# print(sugang)
-# print(sugang['db'])
-# print(sugang.get('db'))
-# print(sugang.get('opensource'))
-for subject, professor in sugang.items():
-    print(f'{subject}과목 담당 교수는 {professor}입니다')
-
-for k in sugang.keys():
-    print(k)
-
-for v in sugang.values():
-    print(v)
-
-for s in sugang.items():
-    print(s)
+drinks_foods = {"위스키" : "초콜릿", "와인" : "치즈", "소주" : "삼겹살", "고량주" : "양꼬치"}
+import random
+# drink = input(drinks_food.keys())
+# print(drinks_foods)
+# print(drinks_foods.pop("고랑주"))
+# print(drinks_foods)
+drinks_foods_keys = list(drinks_foods)
+print(drinks_foods_keys)
+print(drinks_foods_keys.pop(0))
+#(drinks_foods_keys)
+random_drink = random.choice(drinks_foods_keys)
+japan_drinks_food = {"사케" : "광어회", "위스키" : "낙곱새"}
+drinks_foods.update(japan_drinks_food)
+drinks_foods_keys = list(drinks_foods)
+while True:
+    random_drink = random.choice(drinks_foods_keys)
+    menu = input(f'다음 술 중에 고르세요\n1. {drinks_foods_keys[0]},2. {drinks_foods_keys[1]},3. {drinks_foods_keys[2]},4. {drinks_foods_keys[3]}, 5. {drinks_foods_keys[4]}, 6. 아무거나, 7. 종료 ')
+    if menu == "1":
+        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
+    elif menu == "2":
+        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
+    elif menu == "3":
+        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
+    elif menu == "4":
+        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
+    elif menu == "5":
+        print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
+    elif menu == "6":
+        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
+    elif menu == "7":
+        break
+        print(f'다음에 또 오세요')
