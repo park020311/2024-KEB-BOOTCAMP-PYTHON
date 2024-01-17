cb7b70cbe418bef48ac58a59defaa96886356406
park020311 <zxwrtygsnm@naver.com>
2024-01-17 13:33:59 +0900

    v0.5 day04

--- day04.py
+++ day04.py
@@ -1,19 +1,11 @@
-# #리스트
-# li=list()
-# print(li)
-# print(list('cat'))
-# print(list(('ready', 'fire', 'aim')))
-subjects = ["데이터베이스","c++","5","Java","Python","Java","9","리눅스"]
-print(subjects[::-1])
-print(subjects)
-#subjects.reverse()
-# subjects.sort(reverse = False)
-copy_subjects = sorted(subjects)
-print(subjects)
-print(copy_subjects)
-#subjects.remove("Java") # remove는 앞에 있는걸 삭제
-#del subjects[2] # del은 원하는 위치에 있는걸 삭제 가능
-
-number = [1,2,3,4]
-number[1:3]=[10,20,30]
-print(number)
+import copy
+# subjects = ["a","b","c"]
+subjects = ["a",["b","c"],"d"]
+a = subjects
+b= subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+print(subjects, a, b, c, d, e)
+subjects[1][1] = "x"
+print(subjects, a, b, c, d, e)
