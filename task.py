4e8920249d73a8b78f77ae34cf4ed893de162c30
park020311 <zxwrtygsnm@naver.com>
2024-01-18 09:25:21 +0900

    v.task answer

--- task.py
+++ task.py
@@ -10,4 +10,20 @@ print(life["animals"].keys())
 print(life["animals"].values())
 print(life["animals"]["cats"]) # 여기서는 왜 values()를 붙이면 안되는지?
 #8.10
-#squares = {range(10):range(10)**2 for range(10)} # 시간이 없어서 집가서 마저 공부
+# 딕셔너리 컴프리헨션 교수님 설명
+
+# univ = 'inha university'
+# count_alphabet={letter:univ.count(letter) for letter in univ} # dictionary comprehension
+# print(count_alphabet)
+
+# univ = 'inha university'
+# count_alphabet= dict()
+# for letter in univ:
+#     count_alphabet[letter] = univ.count(letter)
+# print(count_alphabet)
+
+#Assignment 8.10
+# squares = {n:pow(n,2) for n in range(10)}
+# squares = {n:n*n for n in range(10)}
+squares = {n:n**2 for n in range(10)}
+print(squares)
