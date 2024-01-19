6948224ed2c27aa18745041b51cb8c20e2f09dc4
park020311 <zxwrtygsnm@naver.com>
2024-01-19 11:12:52 +0900

    v0.3 day06

--- day06.py
+++ day06.py
@@ -1,6 +1,9 @@
 #import random
 import random
 
+class OopsException(Exception):
+    pass
+
 # numbers = list()
 # for i in range(5):
 #     numbers.append(random.randint(1,100))
@@ -9,9 +12,14 @@ print(numbers)
 try:
     pick = int(input(f"Input index (0~{len(numbers)-1}) : "))
     print(numbers[pick]) # numbers는 총 0~4 까지 할당되었기 때문에 5 이상을 입력하면 에러
+    raise OopsException("Oops")
 except IndexError as err:
     print(f"Out of range : Wrong index number\n{err}")
 except ValueError as err:
     print(f"Input Only Number !\n{err}")
-except Exception:
-    print("Error occurs")
+# except OopsException as err:
+#     print(f"Oops Oops {err}")
+except Exception as err:
+    print(f"Error occurs\n{err}")
+else:
+    print(f"program terminate")
