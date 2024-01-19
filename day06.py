129b37fb7dcb7c2aa4b98256390fb5c99bd90d3b
park020311 <zxwrtygsnm@naver.com>
2024-01-19 10:35:09 +0900

    v0.1 day06

---
+++ day06.py
@@ -0,0 +1,24 @@
+def factorial_repetition(n)->int:
+    """
+    반복문을 이용한 팩토리얼 함수
+    :param n: 정수
+    :return: 팩토리얼 값
+    """
+    result = 1
+    for i in range(2, n+1):
+        result = result * i
+    return result
+
+def factorial_recursion(n):
+    """
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: function
+    """
+    if n == 1:
+        return n
+    else:
+        return  n* factorial_repetition(n-1)
+print(factorial_repetition(int(input("number : "))))
+print(factorial_recursion(int(input("number : "))))
+print(globals())
