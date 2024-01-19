842e2ef4258d5bf14b782416459a3f3c83652516
park020311 <zxwrtygsnm@naver.com>
2024-01-19 11:23:40 +0900

    v0.4 day06

--- day06.py
+++ day06.py
@@ -1,25 +1,13 @@
-#import random
-import random
+def desc(f):
+    def wrapper():
+        print("study")
+        f()
+    # print("a")
+    return wrapper
 
-class OopsException(Exception):
-    pass
-
-# numbers = list()
-# for i in range(5):
-#     numbers.append(random.randint(1,100))
-numbers = [random.randint(1,100) for i in range(5)]
-print(numbers)
-try:
-    pick = int(input(f"Input index (0~{len(numbers)-1}) : "))
-    print(numbers[pick]) # numbers는 총 0~4 까지 할당되었기 때문에 5 이상을 입력하면 에러
-    raise OopsException("Oops")
-except IndexError as err:
-    print(f"Out of range : Wrong index number\n{err}")
-except ValueError as err:
-    print(f"Input Only Number !\n{err}")
-# except OopsException as err:
-#     print(f"Oops Oops {err}")
-except Exception as err:
-    print(f"Error occurs\n{err}")
-else:
-    print(f"program terminate")
+@desc
+def something():
+    print("do something~")
+# s=desc(something)
+# s()
+something()
