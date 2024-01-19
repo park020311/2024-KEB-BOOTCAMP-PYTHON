f0a103a04f6c797c1bd31b64417b584c32875206
park020311 <zxwrtygsnm@naver.com>
2024-01-19 14:43:37 +0900

    v0.6 day06

--- day06.py
+++ day06.py
@@ -1,24 +1,29 @@
-# class Pokemon:
-#     pass
-#
-# pikachu = Pokemon()
-# squirtle = Pokemon()
-# pikachu.name = "피카츄"
-# pikachu.nemesis = squirtle
-# print(pikachu.name)
-# # print(pikachu.nemesis.name)
-# pikachu.nemesis.name="꼬부기" #squirtle.name = "꼬부기"
-# print(pikachu.nemesis.name)
-
 class Pokemon:
     def __init__(self,name):
-        self.name = name
-        print(f"{name} 포켓몬스터 생성")
-    def attack(self, target):
+        self.name=name
+    def attack(self,target):
         print(f"{self.name}이(가) {target.name}을(를) 공격!")
-charizard = Pokemon("리자몽")
-pikachu = Pokemon("피카츄")
-squirtle= Pokemon("꼬부기")
-charizard.attack(squirtle)
-print(pikachu.name)
-print(squirtle.name)
+class Pikachu(Pokemon):
+    def __init__(self,name,type):
+        super().__init__(name)
+        self.type=type
+    def attack(self,target):
+        print(f"{self.name}이(가) {target.name}을(를) 백만볼트 공격!")
+    def electric_info(self):
+        print("전기 계열의 공격을 주로 합니다")
+class Squirtle(Pokemon):
+    pass
+
+
+class Agumon:
+    pass
+
+p1 = Pikachu("피카츄","전기")
+p2 = Squirtle("꼬부기")
+p3 = Pokemon("아무개")
+p1.electric_info()
+p1.attack(p2)
+p2.attack(p1)
+print(p1.name)
+print(issubclass(Pikachu,Pokemon))
+print(issubclass(Pikachu,Pokemon))
